33e4d2cb88351b11004ddaf3090bd07c4381a0d2
treglown <117460408+treglown@users.noreply.github.com>
2023-04-21 11:04:09 +0100

    Weight converter

--- main/if_statements.py
+++ main/if_statements.py
@@ -1,3 +1,5 @@
+# This is if statement practising. 
+
 mark = int(input("What is your achieved grade?"))
 
 if mark >= 85:
@@ -7,3 +9,16 @@ elif 65 <= mark <= 85:
 else: 
     if mark <= 65:
         print("You have achieved a fail")
+
+# This is the weight converter activity, listed below: 
+
+weight_converter = input("What is the unit you want to convert to?")
+
+if weight_converter == "lbs":
+    kg = float(input("What is you weight in kilograms?"))
+    lbs = kg * 2.204
+    print("Converterd weight is", lbs)
+elif weight_converter == "kg":
+    lbs = float(input("What is your weight in pounds?"))
+    kg = lbs / 2.204
+    print("Converted weight is", kg)
