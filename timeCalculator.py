cb8dc1a9390ca9ec68243d1edaaea9580f0909a4
treglown <117460408+treglown@users.noreply.github.com>
2023-04-24 16:39:44 +0100

    Lab 4

---
+++ timeCalculator.py
@@ -0,0 +1,29 @@
+time1 = float(input("Input a time like the following: 00.00 "))
+time2 = float(input("Input a second time like the following: 00.00 "))
+
+print("""
+Time Calculator
+    Add 2 times
+    Find the difference between the 2 times
+    Convert to hours 
+    Convert to minutes
+    Convert Minutes to Time
+    Convert Hours to Time
+    Convert Days to Time
+    Exit
+
+    Enter an option:
+""")
+
+
+t1 = datetime.split(':')
+
+seconds = 86400
+
+hour = seconds // 3600 # Gives the value of 24 which is the hours in a day
+
+minutes = seconds // 60 # Gives the value of 60 which is how many minutes are in an day
+
+
+
+
