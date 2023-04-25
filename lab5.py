5da3ca62ab6e1024cacba179f9ed0e71bfe99cc1
treglown <117460408+treglown@users.noreply.github.com>
2023-04-25 14:16:15 +0100

    Labs

---
+++ lab5.py
@@ -0,0 +1,27 @@
+import statistics
+
+
+data = "100,30,53,67,85,87,50,45,51,72,64,69,59,17,22,23,44,25,16,67,85,87,50,45,51,72,59,14,50,55,32,23,24,25,37,28,39,30,33,35,40,34,41,43,94,95,59,98,99,44,45,47,48,49,53,61,63,69,75,77,60,83"
+
+
+grades = data.split(',')
+
+grades = list(map(int, grades))
+
+grades = sorted(grades)
+
+min_grades = min(grades)
+
+max_grades = max(grades)
+
+total_grades = sum(grades)
+average_grades = float(total_grades // len(grades))
+
+median_grades = statistics.median(grades)
+mean_grades = statistics.mean(grades)
+
+txt = "median grade: {}/nmaximum grade:{}/naverage grade: {}/nMean: {} "
+
+print(txt)
+
+
--- lab6.py
+++ lab6.py
@@ -6,7 +6,7 @@ def tax_calculator(pay, percentage):
 def getIncomeTax(income: float)->float:
     if income <= personal_allowance:
         return 0 
-    elif  income <= 34500:
+    elif  income > personal_allowance and income <34500:
         return tax_calculator (income, 20) 
     elif income > 34500 and income < 150000:
         return tax_calculator(income, 40) 
@@ -18,5 +18,3 @@ def getIncomeTax(income: float)->float:
 income = float(input("Input your annual income: £")) - personal_allowance
 calculated_tax = getIncomeTax(income) 
 print(f"Your annual tax is £{calculated_tax}")  
-
-
