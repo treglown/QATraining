c0d5445eaee0c1822b7280f2c97ee1d2dbeb98dc
treglown <117460408+treglown@users.noreply.github.com>
2023-09-28 14:30:41 +0100

    Changed shell path

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Build'){
             steps {
-                sh "secretScript.sh"
+                sh ./secretScript.sh
             }
         }
         stage('Test'){
